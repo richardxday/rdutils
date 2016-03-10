d2a6c40a0e32bddeaa1b3def722b9ba17a52a61b
Richard Day <r.day80@gmail.com>
2016-03-10 21:24:55 +0000

    Add simple command server/sender

---
+++ src/cmdserver.cpp
@@ -0,0 +1,57 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <rdlib/QuitHandler.h>
+#include <rdlib/SettingsHandler.h>
+#include <rdlib/StdSocket.h>
+
+AQuitHandler quithandler;
+
+int main(int argc, char *argv[])
+{
+	AString filename = AString(getenv("HOME")).CatPath("cmdserver");
+	int i;
+
+	for (i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-c") == 0) filename = AString(argv[i]).Prefix();
+	}
+	
+	ASettingsHandler settings(filename, false);
+	ASocketServer    server;
+	AStdSocket       socket(server);
+
+	if (socket.open(settings.Get("addr", "0.0.0.0"),
+					(uint_t)settings.Get("port", "1722"),
+					ASocketServer::Type_Datagram)) {
+		while (!HasQuit()) {
+			if (settings.CheckRead()) {
+				printf("Configuration updated\n");
+				settings.Read();
+			}
+
+			server.Process(200);
+
+			AString line;
+			while ((socket.bytesavailable() > 0) && (line.ReadLn(socket) >= 0)) {
+				AString cmd;
+
+				if ((cmd = settings.Get(line)).Valid()) {
+					cmd = (cmd
+						   .SearchAndReplace("{date}", ADateTime().DateFormat("%Y-%M-%D"))
+						   .SearchAndReplace("{user}", getenv("LOGNAME"))
+						   .SearchAndReplace("{home}", getenv("HOME")));
+					if (system(cmd + " &") != 0) {
+						fprintf(stderr, "Command '%s' failed\n", line.str());
+					}
+				}
+				else fprintf(stderr, "Unrecognized command '%s'\n", line.str());
+			}
+		}
+		
+		socket.close();
+	}
+	
+	return 0;
+}
