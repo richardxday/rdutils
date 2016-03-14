82e2e2ab6568b77d2c89e931171fc9c815ae855a
Richard Day <r.day80@gmail.com>
2016-03-14 20:21:22 +0000

    Allow port argument to be optional.  Exit app if datagram destination is not valid.

--- src/cmdsender.cpp
+++ src/cmdsender.cpp
@@ -10,19 +10,23 @@ int main(int argc, char *argv[])
 	ASocketServer server;
 	AStdSocket    socket(server);
 
-	if (argc < 3) {
-		fprintf(stderr, "Usage: cmdsender <host> <port>\n");
+	if (argc < 2) {
+		fprintf(stderr, "Usage: cmdsender <host> [<port>]\n");
 		exit(1);
 	}
 	
 	if (socket.open("0.0.0.0",
 					0,
 					ASocketServer::Type_Datagram)) {
-		socket.setdatagramdestination(argv[1], (uint_t)AString(argv[2]));
+		uint_t port = 1722;
+
+		if (argc >= 3) port = (uint_t)AString(argv[2]);
 		
-		AString line;
-		while (line.ReadLn(Stdin) >= 0) {
-			if (socket.printf("%s\n", line.str()) <= 0) break;
+		if (socket.setdatagramdestination(argv[1], port)) {
+			AString line;
+			while (line.ReadLn(Stdin) >= 0) {
+				if (socket.printf("%s\n", line.str()) <= 0) break;
+			}
 		}
 		
 		socket.close();
