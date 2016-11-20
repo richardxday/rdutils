d3c086b45a41d2e5702b265bba3d8b8e4166752c
Richard Day <r.day80@gmail.com>
2016-11-20 21:55:41 +0000

    Run srttracker nicely

--- scripts/checktracker.sh
+++ scripts/checktracker.sh
@@ -5,6 +5,6 @@ test -e list.txt || touch list.txt
 ls -1 *.srt >newlist.txt
 if ! diff list.txt newlist.txt >/dev/null 2>&1 ; then
 	echo "Updating files"
-	srttracker .
+	nice srttracker .
 	cp newlist.txt list.txt
 fi
