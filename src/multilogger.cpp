d5492c9299ac0e5c3860b1c24a3a7a5637a36e0c
Richard Day <r.day80@gmail.com>
2019-10-02 21:19:14 +0100

    Improve multilogger by allowing more options

--- src/multilogger.cpp
+++ src/multilogger.cpp
@@ -11,17 +11,44 @@
 int main(int argc, const char *argv[])
 {
 	AStdFile fp;
+	int arg;
 
 	if (argc < 2) {
-		fprintf(stderr, "Usage: multilogger <file-pattern>\n");
+		fprintf(stderr, "Usage: multilogger [{options}] <file-pattern>\n");
+		fprintf(stderr, "Where {options} is one or more of:\n");
+		fprintf(stderr, "\t--time\t\t\tUse time as header for log lines\n");
+		fprintf(stderr, "\t--datetime\t\tUse date and time as header for log lines (ISO-8601 format)\n");
+		fprintf(stderr, "\t--format <format>\tUse specified date format as header for log lines\n");
+		fprintf(stderr, "\t--fileformat <format>\tUse specified date format as format for filename\n");
 		exit(1);
 	}
 
-	AString pattern = argv[1], datestr;
-	AString str;
+	AString dateformat;
+	AString filedateformat = "%Y-%M-%D";
+	for (arg = 1; arg < (argc - 1); arg++) {
+		if (stricmp(argv[arg], "--time") == 0) {
+			dateformat = "%h:%m:%s";
+		}
+		else if (stricmp(argv[arg], "--datetime") == 0) {
+			dateformat = "%Y-%M-%D %h:%m:%s";
+		}
+		else if (stricmp(argv[arg], "--format") == 0) {
+			dateformat = argv[++arg];
+		}
+		else if (stricmp(argv[arg], "--fileformat") == 0) {
+			filedateformat = argv[++arg];
+		}
+		else {
+			fprintf(stderr, "Unknown option '%s'\n", argv[arg]);
+			exit(1);
+		}
+	}
+
+	AString pattern = argv[arg];
+	AString str, datestr;
 	while (!HasQuit() && (str.ReadLn(Stdin) >= 0)) {
 		ADateTime dt = ADateTime().TimeStamp();
-		AString newdatestr = dt.DateFormat("%Y-%M-%D");
+		AString newdatestr = dt.DateFormat(filedateformat);
 
 		if (!fp.isopen() || (newdatestr != datestr)) {
 			fp.close();
@@ -36,7 +63,7 @@ int main(int argc, const char *argv[])
 			}
 		}
 
-		fp.printf("%s: %s\n", dt.DateFormat("%h:%m:%s").str(), str.str());
+		fp.printf("%s%s%s\n", dt.DateFormat(dateformat).str(), dateformat.Valid() ? ": " : "", str.str());
 		fp.flush();
 	}
 
