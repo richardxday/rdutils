0a9562fad51965771828d27eb92bacef5be45245
Richard Day <r.day80@gmail.com>
2019-10-19 23:20:34 +0100

    Move to using new project system for emacs in .project.el.

---
+++ .project.el
@@ -0,0 +1,18 @@
+(c++-mode . ((filetypes . (cpp-project))
+	     (includes . ("accounts"
+			  "agrep"
+			  "cmdsender"
+			  "cmdserver"
+			  "countsmswords"
+			  "etagswrapper"
+			  "imagediff"
+			  "list"
+			  "uri2uri"))
+	     (flags . ("-std=c++11"))
+	     (warnings . ("all"
+			  "extra"))
+	     (packages . ("rdlib-0.1"))
+	     (sources . ("src"
+			 "../rdlib/include"
+			 "../rdlib/src"))
+	     (buildcmd . "make -f makefile")))
